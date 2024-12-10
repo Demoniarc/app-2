493a71600187b7bcaeb6e370e212bd844a9784b0
Tom <107487368+Demoniarc@users.noreply.github.com>
2024-12-10 18:35:28 +0100

    Add files via upload

---
+++ lib/constants.ts
@@ -0,0 +1,13 @@
+export const POLYGON_CHAIN_ID = '0x89'
+
+export const POLYGON_CHAIN_CONFIG = {
+  chainId: POLYGON_CHAIN_ID,
+  chainName: 'Polygon Mainnet',
+  nativeCurrency: {
+    name: 'MATIC',
+    symbol: 'MATIC',
+    decimals: 18
+  },
+  rpcUrls: ['https://polygon-rpc.com/'],
+  blockExplorerUrls: ['https://polygonscan.com/']
+}
