e0c1b88d7302eb2fa84de66d96798529916df7f2
Tom <107487368+Demoniarc@users.noreply.github.com>
2024-12-11 15:07:01 +0100

    Add files via upload

--- lib/constants.ts
+++ lib/constants.ts
@@ -11,3 +11,34 @@ export const POLYGON_CHAIN_CONFIG = {
   rpcUrls: ['https://polygon-rpc.com/'],
   blockExplorerUrls: ['https://polygonscan.com/']
 }
+
+export const API_CONTRACT_ADDRESS = '0xcbD7cDEBC30E2673925304199b4c7545dafA425E'
+
+export const API_CONTRACT_ABI = [
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "NumberOfMonths",
+        "type": "uint256"
+      }
+    ],
+    "name": "payForAccess",
+    "outputs": [],
+    "stateMutability": "payable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "price",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
+]
